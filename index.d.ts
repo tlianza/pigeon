1e1fc71c493838508d7a79cae0559cf6f6053923
Robert Cepa <rcepa@cloudflare.com>
2020-02-21 16:28:46 -0800

    Add TS declaration file

---
+++ index.d.ts
@@ -0,0 +1,27 @@
+export {
+  addGlobalEventProcessor,
+  addBreadcrumb,
+  captureException,
+  captureEvent,
+  captureMessage,
+  configureScope,
+  getHubFromCarrier,
+  getCurrentHub,
+  Hub,
+  Scope,
+  setContext,
+  setExtra,
+  setExtras,
+  setTag,
+  setTags,
+  setUser,
+  withScope
+} from "@sentry/core";
+
+import { initAndBind } from "@sentry/core";
+
+type InitOpts = Parameters<typeof initAndBind>[1] & { event: FetchEvent };
+
+export function init(opts: InitOpts): void;
+export function flush(timeout: number): Promise<void>;
+export function close(timeout: number): Promise<void>;
