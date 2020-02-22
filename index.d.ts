7aed88f6604377383902ee5212ba9f470218dd69
Robert Cepa <rcepa@cloudflare.com>
2020-02-21 17:29:17 -0800

    add some commments

--- index.d.ts
+++ index.d.ts
@@ -23,5 +23,19 @@ import { initAndBind } from "@sentry/core";
 type InitOpts = Parameters<typeof initAndBind>[1] & { event: FetchEvent };
 
 export function init(opts: InitOpts): void;
+
+/**
+ * A promise that resolves when all current events have been sent.
+ * If you provide a timeout and the queue takes longer to drain the promise returns false.
+ *
+ * @param timeout Maximum time in ms the client should wait.
+ */
 export function flush(timeout?: number): Promise<void>;
+
+/**
+ * A promise that resolves when all current events have been sent.
+ * If you provide a timeout and the queue takes longer to drain the promise returns false.
+ *
+ * @param timeout Maximum time in ms the client should wait.
+ */
 export function close(timeout?: number): Promise<void>;
